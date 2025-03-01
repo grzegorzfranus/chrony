517ee8609ce9657b8f17f9e2ac99837754d0e8d4
Grzegorz Franus <142183920+grzegorzfranus@users.noreply.github.com>
2025-03-01 15:18:54 +0100

    retest

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -2,8 +2,21 @@ import pytest
 import testinfra.utils.ansible_runner
 import re
 
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    '.molecule/ansible_inventory').get_hosts('all')
+
+def get_chrony_package_name(host):
+    os_family = host.system_info.distribution
+    if os_family in ["debian", "ubuntu"]:
+        return "chrony"
+    elif os_family in ["centos", "rocky", "redhat"]:
+        return "chrony"
+    else:
+        raise ValueError(f"Unsupported distribution: {os_family}")
+
 def test_chrony_is_installed(host):
-    chrony = host.package("chrony")
+    package_name = get_chrony_package_name(host)
+    chrony = host.package(package_name)
     assert chrony.is_installed
     print(chrony.version)
 
